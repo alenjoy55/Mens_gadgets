ab100ef3c81a41ae80493bfd453509969cce9a55
alenjoy55 <alen85292@gmail.com>
2024-12-10 11:05:50 +0530

    new pro

---
+++ M_gadgets/app/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from .models import *
+import os
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
+# Create your views here.
+def shop_login(req):
+    if 'shop' in req.session:
+        return redirect(shop_home)
+    if 'user' in req.session:
+        return redirect(user_home)
+    else:
+
+        if req.method=='POST':
+            uname=req.POST['uname']
+            password=req.POST['password']
+            data=authenticate(username=uname,password=password)
+            if data:
+                login(req,data)
+                if data.is_superuser:
+                    req.session['shop']=uname
+                    return redirect(shop_home)
+                else:
+                    req.session['user']=uname
+                    return redirect(user_home)
+            else:
+                messages.warning(req, "invaild uname or password")
+            return redirect(shop_login)
+        else:
+            return render(req,'login.html')
+        
+def shop_home(req):
+     return render(req,'shop/shop_home.html')
+
+
+def user_home(req):
+    return render(req,'user/user_home.html')
